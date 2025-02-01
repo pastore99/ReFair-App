a974291e4bcba46b7f87d1585773dc53d203e497
NicoUni99997 <n.laurino1@studenti.unisa.it>
2025-02-01 15:58:01 +0100

    Update call to feedback service in web client

--- start_service.bat
+++ start_service.bat
@@ -5,8 +5,10 @@ start cmd /k "cd /d %~dp0\refair-server && .\env\Scripts\activate && cd.. && py 
 timeout /t 2
 start cmd /k "cd /d %~dp0\refair-server && .\env\Scripts\activate && cd.. && py .\microservices\task_prediction_service.py"
 timeout /t 2
+start cmd /k "cd /d %~dp0\refair-server && .\env\Scripts\activate && cd.. && py .\microservices\feedback_service.py"
+timeout /t 2
 start cmd /k "cd /d %~dp0\refair-server && .\env\Scripts\activate && cd.. && py .\api_gateway\api_gateway.py"
 timeout /t 2
 start cmd /k "cd /d %~dp0\refair-client && npm run dev"
 timeout /t 5
-start "" "http://localhost:5174/"
+start "" "http://localhost:5173/"
