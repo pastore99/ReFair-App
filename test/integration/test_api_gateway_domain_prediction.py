b4f67e65df216e9d904436dbfa795b64f40d6826
NicoUni99997 <n.laurino1@studenti.unisa.it>
2025-02-15 14:26:21 +0100

    Update sonarcloud.yml

--- test/integration/test_api_gateway_domain_prediction.py
+++ test/integration/test_api_gateway_domain_prediction.py
@@ -49,16 +49,11 @@ def test_predict_domain_invalid_json(client):
 def test_predict_domain_missing_user_story(client):
     """ Testa l'errore quando manca il campo 'user_story' """
 
-    response = client.post("/predict/domain", json={})
-
-    # Stampa la risposta per capire meglio il problema
-    print("\nResponse JSON:", response.json)
-    print("Status Code:", response.status_code)
+    response = client.post("/predict/domain", json={"user_story": ""})
 
     assert response.status_code == 400
     assert response.json["status"] == "failure"
     assert "Missing 'user_story'" in response.json["motivation"]
-
 
 def test_predict_domain_service_down(client, mock_requests_post):
     """ Testa il comportamento se il microservizio Domain Prediction non risponde """
